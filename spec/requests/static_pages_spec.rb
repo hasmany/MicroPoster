a76e08e135bff7082b876c1a10d4fc2b14d3efa8
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-07-15 15:44:01 -0500

    title for static pages and test or dynamic header

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
   describe "Home page" do
 
@@ -11,7 +14,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -25,7 +28,7 @@ describe "Static pages" do
 
     it "should have the title 'Title'" do
       visit "/static_pages/help"
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Title")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -38,14 +41,24 @@ describe "Static pages" do
 
     it "should have the title 'About Us" do
       visit "/static_pages/about"
-      expect(page).to have_title('Title')
+      expect(page).to have_title("#{base_title} | About")
     end
 
   end
 
+  describe "Contact page" do
 
+    it "should have the content 'Contact'" do
+      visit "/static_pages/contact"
+      expect(page).to have_content("Contact")
+    end
 
+    it "should have a title 'Contact Us'" do
+      visit "/static_pages/contact"
+      expect(page).to have_title("#{base_title} | Contact")
+    end
 
+  end
 
 end
 
