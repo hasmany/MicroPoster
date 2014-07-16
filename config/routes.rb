3bccda4137974dc2ddfe61f101c08d6e0eba21e3
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-07-16 16:48:35 -0500

    added users controller and refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+
+
+  root  'static_pages#home'
+  get "/signup", to: "users#new", as: 'signup'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/help', to: 'static_pages#help', as: 'help'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
