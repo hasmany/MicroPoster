67f59c6d57e8061a9e7acb898496b7e4f3ada425
hubertanguyen@gmail.com <hubertanguyen@gmail.com>
2014-07-18 22:24:47 -0500

    added token attribute to users table, for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:sessions][:email].downcase)
     if user && user.authenticate(params[:sesssions][:password])
       #Sign user in and redirect to the user's show page
+      sign_in user
+      redirect_to user
     else
+      flash.now[:error] = "Invalid email/password combindation"
       # create an error message and re-render the signin form
-    render 'new'
+      render 'new'
     end
   end
 
